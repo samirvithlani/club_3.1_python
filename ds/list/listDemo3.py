0e74e6293981778f4aa098f250c37eee2b159108
samirvithlani <samir.vithlani83955@gmail.com>
2025-08-08 14:16:35 +0530

    list demo

---
+++ ds/list/listDemo3.py
@@ -0,0 +1,9 @@
+#append,extends,insert,pop,remove
+
+data = [100,200,300,400,500,300]
+
+#2000 list  index --find..
+x = data.index(200)
+print(f"index = {x}")
+cnt = data.count(300)
+print(cnt)
---
+++ ds/list/listDemo4.py
@@ -0,0 +1,12 @@
+data = [100,400,300,200,500,300]
+
+#data.sort()
+data.sort(reverse=True)
+print(data)
+
+
+
+data = ["ram","shyam","amit","Sumit","seeta","geeta","ajay"]
+#data.sort()
+data.sort(key=len,reverse=True)
+print(data)
