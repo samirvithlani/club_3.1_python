be56dee7585464fcb4bf36342f6bb549373bb5c0
samirvithlani <samir.vithlani83955@gmail.com>
2025-09-05 17:53:15 +0530

    set

---
+++ ds/set/SetDemo1.py
@@ -0,0 +1,24 @@
+data = {100,20,30,100,40,45,67,89}
+print(data)
+
+data.add(999)
+print(data)
+
+#data.update([999,78,56])
+data.update({999,78,56})
+print(data)
+
+removedElm = data.pop()
+print(f"removing element {removedElm}")
+print(removedElm)
+
+data.remove(30)
+print(data)
+
+#data.clear()
+
+#print(data[0])
+
+for i in data:
+    print(i)
+
